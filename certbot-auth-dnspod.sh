a6df85b8cf9bacb9df2abc6d81b104d05a524eab
Alone <hi@anlo.ng>
2019-06-27 17:57:28 +0800

    201906271757 支持多级子域名

--- certbot-auth-dnspod.sh
+++ certbot-auth-dnspod.sh
@@ -11,7 +11,10 @@ API_TOKEN=""
 
 USER_AGENT="AnDNS/1.0.0 (hi@anlo.ng)"
 DOMAIN=$(expr match "$CERTBOT_DOMAIN" '.*\.\(.*\..*\)')
+TXHOST=$(expr match "$CERTBOT_DOMAIN" '\(.*\)\..*\..*')
 [ -z "$DOMAIN" ] && DOMAIN="$CERTBOT_DOMAIN"
+[ -z "$TXHOST" ] || TXHOST="_acme-challenge.$TXHOST"
+[ -z "$TXHOST" ] && TXHOST="_acme-challenge"
 
 if [ -z "$API_TOKEN" ]; then
     [ -f $HOME/.dnspod_token_$DOMAIN ] && API_TOKEN=$(cat $HOME/.dnspod_token_$DOMAIN)
@@ -38,6 +41,7 @@ PARAMS="login_token=$API_TOKEN&format=json"
 echo "\
 CERTBOT_DOMAIN: $CERTBOT_DOMAIN
 DOMAIN:         $DOMAIN
+TXHOST:         $TXHOST
 VALIDATION:     $CERTBOT_VALIDATION"
 #echo "PARAMS:         $PARAMS"
 
@@ -51,7 +55,7 @@ fi
 
 RECORDS=$(curl -s -X POST "https://dnsapi.cn/Record.List" \
     -H "User-Agent: $USER_AGENT" \
-    -d "$PARAMS&domain=$CERTBOT_DOMAIN&keyword=_acme-challenge" \
+    -d "$PARAMS&domain=$DOMAIN&keyword=$TXHOST" \
 | python -c "import sys,json;ret=json.load(sys.stdin);print(ret.get('records',[{}])[0].get('id',''))")
 
 echo "\
@@ -63,12 +67,12 @@ if [ -n "$RECORDS" ]; then
     RECORD_ID="$RECORDS"
     RECORD_ID=$(curl -s -X POST "https://dnsapi.cn/Record.Modify" \
         -H "User-Agent: $USER_AGENT" \
-        -d "$PARAMS&domain=$CERTBOT_DOMAIN&sub_domain=_acme-challenge&record_type=TXT&value=$CERTBOT_VALIDATION&record_line=默认&record_id=$RECORD_ID" \
+        -d "$PARAMS&domain=$DOMAIN&sub_domain=$TXHOST&record_type=TXT&value=$CERTBOT_VALIDATION&record_line=默认&record_id=$RECORD_ID" \
     | python -c "import sys,json;ret=json.load(sys.stdin);print(ret.get('record',{}).get('id',ret.get('status',{}).get('message','error')))")
 else
     RECORD_ID=$(curl -s -X POST "https://dnsapi.cn/Record.Create" \
         -H "User-Agent: $USER_AGENT" \
-        -d "$PARAMS&domain=$CERTBOT_DOMAIN&sub_domain=_acme-challenge&record_type=TXT&value=$CERTBOT_VALIDATION&record_line=默认" \
+        -d "$PARAMS&domain=$DOMAIN&sub_domain=$TXHOST&record_type=TXT&value=$CERTBOT_VALIDATION&record_line=默认" \
     | python -c "import sys,json;ret=json.load(sys.stdin);print(ret.get('record',{}).get('id',ret.get('status',{}).get('message','error')))")
 fi
 
@@ -84,4 +88,4 @@ echo $RECORD_ID > /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID
 echo $CERTBOT_VALIDATION > /tmp/CERTBOT_$CERTBOT_DOMAIN/VALIDATION
 
 # Sleep to make sure the change has time to propagate over to DNS
-sleep 25
+sleep 30
